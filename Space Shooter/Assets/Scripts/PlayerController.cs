a733902b283ed763d0eed41e073105accb53295b
Bailknight <bailknight@hotmail.com>
2017-01-20 02:23:02 +0900

    스패이스슈터 작업중

--- Space Shooter/Assets/Scripts/PlayerController.cs
+++ Space Shooter/Assets/Scripts/PlayerController.cs
@@ -15,12 +15,26 @@ public class PlayerController : MonoBehaviour {
 	public float tilt;
 	public Boundary boundary;
 
+	public GameObject shot;
+	public Transform shotSpawn;
+	public float fireRate;
+	private float nextFire;
+
 	// Use this for initialization
 	void Start () 
 	{
 		rb = GetComponent<Rigidbody>();
 	}
-	
+
+	void Update ()
+	{
+		if (Input.GetButton ("Fire1") && Time.time > nextFire) {
+			nextFire = Time.time + fireRate;
+			Instantiate (shot, shotSpawn.position, shotSpawn.rotation);
+		}
+	}
+		
+
 	void FixedUpdate () 
 	{
 		float moveHorizontal = Input.GetAxis ("Horizontal");
