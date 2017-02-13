b5d7b051c3039f37c9c4a6637256af405b5e9be7
Bailknight <bailknight@hotmail.com>
2017-02-14 00:42:45 +0900

    AI튜토리얼

---
+++ AI tutotrial/Assets/Scripts/FollowDestination.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class FollowDestination : MonoBehaviour {
+
+	private NavMeshAgent thisAgent = null;
+	public Transform destination = null;
+
+
+	void Awake () 
+	{
+		thisAgent = GetComponent<NavMeshAgent> ();
+	}
+	
+
+	void Update () 
+	{
+		thisAgent.SetDestination (destination.position);
+	}
+}
